010d6f0475fb23a4ae7b2355b0934af623199a37
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-07-19 15:43:46 +0200

    Fix platform fade animation

--- scripts/Platform.gd
+++ scripts/Platform.gd
@@ -3,36 +3,35 @@ extends Node3D
 
 var _border: MeshInstance3D
 
-# var _end: bool
-# var _result: bool
-
 
 func _ready():
-	# _tween = self.create_tween()
 	_border = get_node("Border")
 
 	_recolor()
 
 
-# func play_fade_out_animation():
-	# _end = true
-	# var material = _border.get_surface_override_material(0) as StandardMaterial3D
-
-	# _result = _tween.stop_all()
-	# _play_tween_animation(material, 1, 0)
+func play_fade_out_animation():
+	var material = _border.get_surface_override_material(0) as StandardMaterial3D
+	
+	_play_tween_animation(material, 0, true)
 
 
 func _recolor():
 	var material: StandardMaterial3D = Globals.get_emission_material(0)
-	_border.set_surface_override_material(0, material)
+	material.emission_energy_multiplier = 0
 	
-	# _play_tween_animation(material, 0, 1)
+	_border.set_surface_override_material(0, material)
+	_play_tween_animation(material, 1)
 
 
-# func _play_tween_animation(material, startValue, endValue):
-# 	var speed = Globals.animation_speed
-# 	speed -= 0.05
-
-# 	_result = _tween.interpolate_value(material, "emission_energy",
-# 			startValue, endValue, speed, Tween.TRANS_SINE)
-# 	_result = _tween.start()
+func _play_tween_animation(material, finalValue, destroy = false):
+	var speed = Globals.animation_speed
+	speed -= 0.05
+	
+	var tween = self.create_tween()
+	
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(material, "emission_energy_multiplier", finalValue, speed)
+	
+	if destroy:
+		tween.tween_callback(func(): self.get_parent().queue_free())
