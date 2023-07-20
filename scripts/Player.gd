a2b36dac5a4d5905b14733983a61567fba077206
Mikołaj Łukawski <lukawski.mikolaj@gmail.com>
2023-07-20 15:51:22 +0200

    Fix camera rotation animation

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -46,25 +46,17 @@ func play_spatial_animation(animation):
 	_bounce_player.play(animation)
 
 
-# func rotate_camera_by(rotations):
-# 	var clockwise = Globals.get_random_bool()
+func rotate_camera_by(rotations):
+	var clockwise = Globals.get_random_bool()
 
-# 	if clockwise:
-# 		Globals.camera_rotation += rotations
-# 	else:
-# 		Globals.camera_rotation -= rotations
+	if clockwise:
+		Globals.camera_rotation += rotations
+	else:
+		Globals.camera_rotation -= rotations
 
-# 	# Globals.camera_rotation = posmod(Globals.camera_rotation, 3)
-# 	# Can improve
+	Globals.camera_rotation = posmod(Globals.camera_rotation, 4)
 
-# 	if Globals.camera_rotation > 3:
-# 		Globals.camera_rotation -= 4
-
-# 	if Globals.camera_rotation < 0:
-# 		Globals.camera_rotation += 4
-
-# 	_level.toggle_controls(false)
-# 	_play_camera_rotation_animation(clockwise, rotations)
+	_play_camera_rotation_animation(clockwise, rotations)
 
 
 # func _play_game_over_animation(animation):
@@ -74,43 +66,27 @@ func play_spatial_animation(animation):
 # 	_animation.play(animation)
 
 
-# func _play_camera_rotation_animation(clockwise, rotations):
-# 	var rotate_by = 90 * rotations
-# 	# More rotations take longer
-# 	var time = rotations * Globals.animation_speed * 1.5
+func _play_camera_rotation_animation(clockwise, rotations):
+	var rotate_by = 90 * rotations
+	# More rotations take longer
+	var time = rotations * Globals.animation_speed * 1.5
 
-# 	var new_rotation = self.rotation_degrees
+	var new_rotation = self.rotation_degrees
 
-# 	if clockwise:
-# 		new_rotation.y += rotate_by
-# 	else:
-# 		new_rotation.y -= rotate_by
-
-# 	_play_tween_animation("rotation_degrees", new_rotation, time)
-
-
-# func _play_tween_animation(type, new_vector, time):
-# 	_result = _tween.interpolate_property(self, type, null,
-# 			new_vector, time, Tween.TRANS_SINE)
-# 	_result = _tween.start()
+	if clockwise:
+		new_rotation.y += rotate_by
+	else:
+		new_rotation.y -= rotate_by
+		
+	var tween = self.create_tween()
+	
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "rotation_degrees", new_rotation, time)
+	tween.tween_callback(func(): Globals.player_can_move = true)
 
 
 # func _is_game_over_animation(animation) -> bool:
 # 	return animation == "camera_up" or animation == "camera_up_long"
-
-
-# # Reenable controls
-# func _on_tween_animation_finished():
-# 	# Update global position at the end of animation
-# 	Globals.player_position = self.position
-# 	_level.toggle_controls(true)
-
-
-# func _on_spatial_animation_finished(animation):
-# 	if _is_game_over_animation(animation):
-# 		_result = get_tree().reload_current_scene()
-# 	else:
-# 		_level.toggle_controls(true)
 
 
 func _on_bounce_player_animation_finished(_anim_name):
